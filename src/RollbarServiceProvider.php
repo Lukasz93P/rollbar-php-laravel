597ec80893bad91ad38b5e39e92a5330385e578f
Lukasz Franikowski <lfranikowski@getresponse.com>
2021-11-05 21:40:40 +0100

    Removed handler nullable fix

--- src/RollbarServiceProvider.php
+++ src/RollbarServiceProvider.php
@@ -42,7 +42,6 @@ class RollbarServiceProvider extends ServiceProvider
             $handleError = (bool) Arr::pull($config, 'handle_error');
             $handleFatal = (bool) Arr::pull($config, 'handle_fatal');
 
-            $config['handler'] = $config['rollbar_handler'] ?? null;
             Rollbar::init($config, $handleException, $handleError, $handleFatal);
 
             return Rollbar::logger();
